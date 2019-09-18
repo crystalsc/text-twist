d31c9fa773f385bb8a2c0c969537daf240ba469c
Crystal Conroy <csconroy@udel.edu>
2019-09-18 19:18:54 -0400

    dsf

--- api.php
+++ api.php
@@ -1,5 +1,4 @@
 <?php
-
         $verb = $_SERVER["REQUEST_METHOD"];
         
         if ($verb == "GET"){
@@ -17,6 +16,12 @@
             } else {
                 $query = "SELECT rack, weight, words FROM racks where weight <= 15 order by random() limit 1";
             }
+            // $query = "CREATE TABLE results (
+            //     date_submitted TEXT PRIMARY KEY,
+            //     user TEXT NOT NULL,
+            //     score INTEGER NOT NULL
+            // )";
+            // $query = "SELECT rack, weight, words FROM racks where weight > 30 order by random() limit 1";
             $statement = $dbhandle->prepare($query);
             $statement->execute();
             $results = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -24,27 +29,26 @@
             header('Content-Type: application/json');
             echo json_encode($results);
         } else if ($verb == "POST"){
-            // $user = "anonymous";
-            // $score = 0;
-            // $date = 0;
-            // if (isset($_POST["user"])){
-            //     $user = $_POST["user"];
-            // }
-            // if (isset($_POST["score"])){
-            //     $score = $_POST["score"];
-            // }
-            // if (isset($_POST["date"])){
-            //     $date = $_POST["date"];
-            // }
-            // $query = "INSERT INTO results (user,score,date_submitted) VALUES($user,$score,$date)";
-            // $statement = $dbhandle->prepare($query);
-            // $statement->execute();
-            // $results = $statement->fetchAll(PDO::FETCH_ASSOC);
-            // header('HTTP/1.1 200 OK');
-            // header('Content-Type: application/json');
-            echo "hi":"bye";
+            $user = "anonymous";
+            $score = 0;
+            $date = 0;
+            if (isset($_POST["user"])){
+                $user = $_POST["user"];
+            }
+            if (isset($_POST["score"])){
+                $score = $_POST["score"];
+            }
+            if (isset($_POST["date"])){
+                $date = $_POST["date"];
+            }
+            $query = "INSERT INTO results (user,score,date_submitted) VALUES($user,$score,$date)";
+            $statement = $dbhandle->prepare($query);
+            $statement->execute();
+            $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+            header('HTTP/1.1 200 OK');
+            header('Content-Type: application/json');
+            echo json_encode($results);
         } else {
             echo "USAGE GET or POST";
         }
-
 ?>
