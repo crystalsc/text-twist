7258c71b755a2bdbadf05f6c673ab3bbe48ae293
Crystal Conroy <csconroy@udel.edu>
2019-09-17 20:48:47 -0400

    q

--- api.php
+++ api.php
@@ -11,7 +11,7 @@
             //the limit 0, 10 takes the first 10 results.
             // you might want to consider taking more results, implementing "pagination", 
             // ordering by rank, etc.
-            $query = "SELECT rack, words FROM racks order by random() limit 1";
+            $query = "SELECT rack, weight, words FROM racks order by random() limit 1";
             
             //this next line could actually be used to provide user_given input to the query to 
             //avoid SQL injection attacks
