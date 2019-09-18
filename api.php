f9caf5960da3961e16fdb0d7e7c642774f5e85ef
Crystal Conroy <csconroy@udel.edu>
2019-09-18 13:45:40 -0400

    s

--- api.php
+++ api.php
@@ -5,18 +5,19 @@
         if ($verb == "GET"){
             $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
             if (!$dbhandle) die ($error);
-            if (isset($_GET['mode'])) {
-                if ($_GET['mode']=="HARD") {
-                    $query = "SELECT rack, max(weight), words where weight > 10 FROM racks order by random() limit 1";
-                }
-                else if ($_GET['mode']=="MED") {
-                    $query = "SELECT rack, weight, words where weight > 5 and weight <= 10 FROM racks order by random() limit 1";
-                } else {
-                    $query = "SELECT rack, weight, words where weight <= 5 FROM racks order by random() limit 1";
-                }
-            } else {
-                $query = "SELECT rack, weight, words where weight <= 5 FROM racks order by random() limit 1";
-            }
+            // if (isset($_GET['mode'])) {
+            //     if ($_GET['mode']=="HARD") {
+            //         $query = "SELECT rack, max(weight), words where weight > 10 FROM racks order by random() limit 1";
+            //     }
+            //     else if ($_GET['mode']=="MED") {
+            //         $query = "SELECT rack, weight, words where weight > 5 and weight <= 10 FROM racks order by random() limit 1";
+            //     } else {
+            //         $query = "SELECT rack, weight, words where weight <= 5 FROM racks order by random() limit 1";
+            //     }
+            // } else {
+            //     $query = "SELECT rack, weight, words where weight <= 5 FROM racks order by random() limit 1";
+            // }
+            $query = "SELECT rack, weight, words FROM racks order by random() limit 1";
             $statement = $dbhandle->prepare($query);
             $statement->execute();
             $results = $statement->fetchAll(PDO::FETCH_ASSOC);
