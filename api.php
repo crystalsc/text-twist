9d05d30a6335f161e817bf04390a1f4482cb72a7
Crystal Conroy <csconroy@udel.edu>
2019-09-18 20:08:09 -0400

    sdf

--- api.php
+++ api.php
@@ -51,10 +51,8 @@ function console_log($output, $with_script_tags = true) {
             //     $date = $_POST["date"];
             // }
             //$query = "INSERT INTO results (user,score,date_submitted) VALUES(" + $user +"," + $score + "," +$date + ")";
-            $query = "insert into results (user,score,date_submitted) values(" . $user . "," . $score + "," . $date . ")";
+            $query = "insert into results (user,score,date_submitted) values(".$user.",".$score.",".$date.")";
             echo $query;
-            echo $user;
-            echo "insert into results (user,score,date_submitted) values("+ $user + "," + $score + "," + $date + ")";
             // $statement = $dbhandle->prepare($query);
             // $statement->execute();
             // $results = $statement->fetchAll(PDO::FETCH_ASSOC);
